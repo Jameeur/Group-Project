0f8cbd41bcb00cfbb19f89da48f495f25e583a21
Shehroz34 <shehraizsafarish12345@gmail.com>
2025-02-19 19:36:03 +0000

    added new interface event calendar.

---
+++ src/main/java/event_calendar.java
@@ -0,0 +1,13 @@
+package ExternalTeamsInterface;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+public interface event_calendar {
+
+    ArrayNode getAvailableTimeSlots(String date);
+
+    void reserveTimeSlot(String date, String startTime, String endTime, String filmTitle);
+
+    void notifyAvailability(String date, String startTime, String endTime);
+
+}
+
