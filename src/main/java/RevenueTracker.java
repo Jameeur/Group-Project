c0279b16eea15fa47ee4aa03f5400b6072ab0b18
Shehroz34 <shehraizsafarish12345@gmail.com>
2025-02-19 20:35:34 +0000

    added new methond in RevenueTracker.java

--- src/main/java/RevenueTracker.java
+++ src/main/java/RevenueTracker.java
@@ -1,7 +1,15 @@
+package ExternalTeamsInterface;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+
 public interface RevenueTracker {
 
     // Retrieves a financial summary for a specific event, including revenue and expenses
     Map<String, BigDecimal> getFinancialSummary(String eventName);
+
+    Map<String, BigDecimal> getRevenueSplit(String startDate, String endDate);
 
 
 }
