3a950dd0215aadf0091ddfb3a7515eeeaa96b7d5
Jameeur <158040961+Jameeur@users.noreply.github.com>
2025-02-22 11:56:06 +0000

    Update EventCalendar.java
    Added comments

--- src/main/java/EventCalendar.java
+++ src/main/java/EventCalendar.java
@@ -3,20 +3,25 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public interface EventCalendar {
 
+    //Retrieves only available time slots for that event
     ArrayNode getAvailableTimeSlots(String date);
 
+    //If an event is booked, it reserves the slot for the user
     void reserveTimeSlot(String date, String startTime, String endTime, String filmTitle);
 
+    //Notifies the user which events are currently available
     void notifyAvailability(String date, String startTime, String endTime);
 
+    //If there is  a cost to pay for the event, this is notified to the user
     double getEventCost(int eventId);
 
+    //Connects to the events database to find the events
     Connection connectToDatabase();
 
-    List<Events> getAvailableEvents();
-
+    //Gets currently available events
     List<Timeslot> getAvailableEvents(String name);
 
+    //User can book an event, followed by an event notification
     boolean bookEvents(int eventId, String eventName)
 
 }
